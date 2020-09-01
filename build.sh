20881afade4322c239c917ffcf57eacb86d85f6c
vivek-adarsh <adarsh.vivek1@gmail.com>
2020-09-01 14:07:56 -0700

    buildx manifests

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+docker buildx create --name version2 --use
+
+docker buildx inspect
+docker buildx inspect --bootstrap
+
 docker buildx build --platform=linux/arm64,linux/amd64,linux/arm/v7 -t vivekhpe/nodeinfo:latest . --push
